801f7e4b862bfaec210502f48e372dde8e19983d
sanopsmx <sanjeev@opsmx.io>
2021-03-08 21:48:35 +0530

    Added code for echo event extension.

--- custom-event-echo/src/main/java/com/opsmx/spinnaker/echo/plugins/EventListenerExtension.java
+++ custom-event-echo/src/main/java/com/opsmx/spinnaker/echo/plugins/EventListenerExtension.java
@@ -1,2 +1,24 @@
-package com.opsmx.spinnaker.echo.plugins;public class EventListenerExtension {
+package com.opsmx.spinnaker.echo.plugins;
+
+
+import com.netflix.spinnaker.echo.api.events.Event;
+import com.netflix.spinnaker.echo.api.events.EventListener;
+import org.pf4j.Extension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Extension
+public class EventListenerExtension implements EventListener {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void processEvent(Event event) {
+        log.info(" process event ");
+        log.info(" event id : " + event.getEventId());
+        log.info(" event payload " + event.getPayload());
+        log.info(" event details " + event.getDetails());
+        log.info(" event content " + event.getContent());
+        log.info(" event rawString " + event.getRawContent());
+    }
 }
