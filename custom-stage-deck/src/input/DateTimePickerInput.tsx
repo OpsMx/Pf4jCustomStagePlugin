68fb396187ae4eb99f9aa43e957cbb3d93a70e1a
rsinghsidhu <rsinghsiddhu@salesforce.com>
2021-06-01 08:54:56 +0530

    Changes related to Date field epoch conversion.

--- custom-stage-deck/src/input/DateTimePickerInput.tsx
+++ custom-stage-deck/src/input/DateTimePickerInput.tsx
@@ -1,27 +1,46 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent } from 'react';
+import { createFakeReactSyntheticEvent, IFormInputProps, OmitControlledInputPropsFrom } from '@spinnaker/core';
 
-export default function DateTimePicker(props: any) {
-  const { date, ...rest } = props;
+export interface IDateInputProps extends IFormInputProps, OmitControlledInputPropsFrom<React.InputHTMLAttributes<any>> {
+  date: number;
+}
 
-  let localTimeToEpoch = (selectedDate: string) => new Date(selectedDate).getTime() / 1000;
+const epochToLocalTime = (epochString: number) => {
+  const inputDate = epochString ? new Date(epochString) : new Date();
+  let hours = inputDate.getHours();
+  let minutes = inputDate.getMinutes();
+  hours = hours % 24;
+  hours = hours ? hours : 24;
+  minutes = minutes < 10 ? +('0' + minutes) : minutes;
+  const year = inputDate.getFullYear();
+  let month = inputDate.getMonth();
+  month = month < 10 ? +('0' + month) : month;
+  let date = inputDate.getDate();
+  date = date < 10 ? +('0' + date) : date;
+  hours = hours < 10 ? +('0' + hours) : hours;
+  minutes = minutes < 10 ? +('0' + minutes) : minutes;
+  const strTime = year + '-' + month + '-' + date + 'T' + hours + ':' + minutes;
+  return strTime;
+};
 
-  let epochToLocalTime = (epochString: number) => {
-    let inputDate = new Date(epochString);
-    var hours = inputDate.getHours();
-    var minutes = inputDate.getMinutes();
-    hours = hours % 24;
-    hours = hours ? hours : 24;
-    minutes = minutes < 10 ? +('0' + minutes) : minutes;
-    var year = inputDate.getFullYear();
-    var month = inputDate.getMonth();
-    month = month < 10 ? +('0' + month) : month;
-    var date = inputDate.getDate();
-    date = date < 10 ? +('0' + date) : date;
-    hours = hours < 10 ? +('0' + hours) : hours;
-    minutes = minutes < 10 ? +('0' + minutes) : minutes;
-    var strTime = year + '-' + month + '-' + date + 'T' + hours + ':' + minutes;
-    return strTime;
-  };
+const localTimeToEpoch = (selectedDate: string) => new Date(selectedDate).getTime() / 1000;
+export class DateTimePicker extends React.Component<IDateInputProps> {
+  public render() {
+    const { date, onChange, name, value, ...rest } = this.props;
 
-  return <input type="datetime-local" value={epocToLocalTime(new Date())} {...props}></input>;
+    const formattedDate = epochToLocalTime(date);
+
+    return (
+      <input
+        type="datetime-local"
+        value={formattedDate}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+          const date = e.target.value;
+          const newValue = date && localTimeToEpoch(date);
+          this.props.onChange(createFakeReactSyntheticEvent({ name, value: newValue }));
+        }}
+        {...rest}
+      ></input>
+    );
+  }
 }
