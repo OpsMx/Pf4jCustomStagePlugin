9f19c5a7bae1ab0954cef4bf450dc55c8374ceec
Luthan95 <luthan@opsmx.io>
2021-07-27 01:15:11 +0530

    new json changes

--- custom-stage-orca/src/main/java/com/opsmx/plugin/stage/custom/ApprovalTask.java
+++ custom-stage-orca/src/main/java/com/opsmx/plugin/stage/custom/ApprovalTask.java
@@ -29,7 +29,6 @@ import com.netflix.spinnaker.orca.api.pipeline.Task;
 import com.netflix.spinnaker.orca.api.pipeline.TaskResult;
 import com.netflix.spinnaker.orca.api.pipeline.models.ExecutionStatus;
 import com.netflix.spinnaker.orca.api.pipeline.models.StageExecution;
-import com.opsmx.plugin.stage.custom.model.CustomConnector;
 
 @Extension
 @PluginComponent
@@ -140,7 +139,7 @@ public class ApprovalTask implements Task {
 			}
 			
 			String gateUrl = (String) jsonContext.get(GATE_URL);
-			logger.info("Application name : {}, pipeline name : {}", stage.getExecution().getApplication(), stage.getExecution().getName());
+			logger.info("Application name : {}, pipeline name : {}, GateUrl : {}", stage.getExecution().getApplication(), stage.getExecution().getName(), gateUrl);
 
 			HttpPost request = new HttpPost(gateUrl);
 			request.setEntity(new StringEntity(preparePayload(jsonContext, stage.getExecution().getId())));
@@ -159,7 +158,8 @@ public class ApprovalTask implements Task {
 			logger.info("visibility approval trigger response : {}", registerResponse);
 
 			if (response.getStatusLine().getStatusCode() != 202) {
-				outputs.put(EXCEPTION, registerResponse);
+				logger.info("Failed to trigger approval request with Status code : {}", response.getStatusLine().getStatusCode());
+				outputs.put(EXCEPTION, String.format("Failed to trigger approval request with Status code : %s", response.getStatusLine().getStatusCode()));
 				return TaskResult.builder(ExecutionStatus.TERMINAL)
 						.context(contextMap)
 						.outputs(outputs)
@@ -233,8 +233,8 @@ public class ApprovalTask implements Task {
 		finalJson.put("approvalCallbackURL", "http://oes-platform:8095/callbackurl");
 		finalJson.put("rejectionCallbackURL", "http://oes-platform:8095/rejectionbackurl");
 		finalJson.put("executionId", executionId);
-
-		String connectorJson = (String) parameterContext.get("connectors");
+		
+		String connectorJson = objectMapper.writeValueAsString(parameterContext.get("connectors"));
 		ArrayNode connectorNode = (ArrayNode) objectMapper.readTree(connectorJson);
 		ArrayNode toolConnectorPayloads = objectMapper.createArrayNode();
 		connectorNode.forEach(connector -> {
@@ -276,9 +276,9 @@ public class ApprovalTask implements Task {
 		ArrayNode parameterArrayNode = objectMapper.createArrayNode();
 		ArrayNode valuesNode = (ArrayNode) connector.get(VALUES);
 		valuesNode.forEach(bambooNode -> {
-			if (bambooNode != null && bambooNode.get(PROJECT_NAME) != null && !bambooNode.get(PROJECT_NAME).asText().isEmpty() 
-					&& bambooNode.get(PLAN_NAME) != null && !bambooNode.get(PLAN_NAME).isEmpty()
-					&& bambooNode.get(BUILD_NUMBER) != null && !bambooNode.get(BUILD_NUMBER).isEmpty()) {
+			if (bambooNode != null && bambooNode.get(PROJECT_NAME) != null && ! (bambooNode.get(PROJECT_NAME).asText()).isEmpty() 
+					&& bambooNode.get(PLAN_NAME) != null && ! bambooNode.get(PLAN_NAME).asText().isEmpty()
+					&& bambooNode.get(BUILD_NUMBER) != null && ! bambooNode.get(BUILD_NUMBER).asText().isEmpty()) {
 					parameterArrayNode.add(objectMapper.createObjectNode()
 							.put(PROJECT_NAME, bambooNode.get(PROJECT_NAME).asText().trim())
 							.put(PLAN_NAME, bambooNode.get(PLAN_NAME).asText().trim())
@@ -298,8 +298,8 @@ public class ApprovalTask implements Task {
 		ArrayNode parameterArrayNode = objectMapper.createArrayNode();
 		ArrayNode valuesNode = (ArrayNode) connector.get(VALUES);
 		valuesNode.forEach(jenkinsNode -> {
-			if (jenkinsNode != null && jenkinsNode.get(JOB) != null && !jenkinsNode.get(JOB).asText().isEmpty() 
-					&& jenkinsNode.get(BUILD_ID) != null && !jenkinsNode.get(BUILD_ID).isEmpty()) {
+			if (jenkinsNode != null && jenkinsNode.get(JOB) != null && ! (jenkinsNode.get(JOB).asText()).isEmpty() 
+					&& jenkinsNode.get(BUILD_ID) != null && ! (jenkinsNode.get(BUILD_ID).asText()).isEmpty()) {
 					parameterArrayNode.add(objectMapper.createObjectNode()
 							.put(JOB, jenkinsNode.get(JOB).asText().trim())
 							.put(BUILD_ID, jenkinsNode.get(BUILD_ID).asText().trim())
@@ -319,7 +319,8 @@ public class ApprovalTask implements Task {
 		ArrayNode parameterArrayNode = objectMapper.createArrayNode();
 		ArrayNode valuesNode = (ArrayNode) connector.get(VALUES);
 		valuesNode.forEach(gitNode -> {
-			if (gitNode != null && gitNode.get(REPO) != null && !gitNode.get(REPO).asText().isEmpty() && gitNode.get(COMMIT_ID) != null && !gitNode.get(COMMIT_ID).isEmpty()) {
+			if (gitNode != null && gitNode.get(REPO) != null && ! gitNode.get(REPO).asText().isEmpty() 
+					&& gitNode.get(COMMIT_ID) != null && ! gitNode.get(COMMIT_ID).asText().isEmpty()) {
 					ArrayNode commitIds = objectMapper.createArrayNode();
 					Arrays.asList(gitNode.get(COMMIT_ID).asText().split(",")).forEach(a -> {
 						commitIds.add(a.trim());
@@ -350,207 +351,4 @@ public class ApprovalTask implements Task {
 		singleObjectNode.set(PARAMETERS, objectMapper.createArrayNode().add(objectMapper.createObjectNode().set(param, paramsNode)));
 		toolConnectorPayloads.add(singleObjectNode);
 	}
-
-//	private String getPayloadString(ApprovalContext context, String executionId) throws Exception {
-//
-//		ObjectNode finalJson = objectMapper.createObjectNode();
-//		finalJson.put("approvalCallbackURL", "http://oes-platform:8095/callbackurl");
-//		finalJson.put("rejectionCallbackURL", "http://oes-platform:8095/rejectionbackurl");
-//		finalJson.put("executionId", executionId);
-//
-//		if (context.getImageIds() != null && !context.getImageIds().isEmpty()) {
-//			ArrayNode images = objectMapper.createArrayNode();
-//			Arrays.asList(context.getImageIds().split(",")).forEach(a -> {
-//				images.add(a.trim());
-//			});
-//			finalJson.set("imageIds", images);
-//		}
-//
-//		ArrayNode toolConnectorPayloads = objectMapper.createArrayNode();
-//		if (context.getJiraId() != null && ! context.getJiraId().isEmpty()) {
-//			ObjectNode jiraObjectNode = objectMapper.createObjectNode();
-//			jiraObjectNode.put(CONNECTOR_TYPE, JIRA);
-//			ArrayNode jiraTickets = objectMapper.createArrayNode();
-//
-//			Arrays.asList(context.getJiraId().split(",")).forEach(a -> {
-//				jiraTickets.add(a.trim());
-//			});
-//
-//			jiraObjectNode.set(PARAMETERS, objectMapper.createArrayNode().add(objectMapper.createObjectNode().set(JIRA_TICKET_NO, jiraTickets)));
-//			toolConnectorPayloads.add(jiraObjectNode);
-//
-//		} else {
-//			logger.info("JIRAID is not provided");
-//		}
-//
-//		if (context.getGit() != null && ! context.getGit().isEmpty()) {
-//			ObjectNode gitObjectNode = objectMapper.createObjectNode();
-//			gitObjectNode.put(CONNECTOR_TYPE, GIT);
-//			ArrayNode parameterArrayNode = objectMapper.createArrayNode();
-//			context.getGit().forEach(git -> {
-//				if (git != null && git.getGitCommitId() != null && !git.getGitCommitId().isEmpty()) {
-//					ArrayNode commitIds = objectMapper.createArrayNode();
-//					Arrays.asList(git.getGitCommitId().split(",")).forEach(a -> {
-//						commitIds.add(a.trim());
-//					});
-//					parameterArrayNode.add(objectMapper.createObjectNode().put(REPO, git.getGitRepo()).set(COMMIT_ID, commitIds));
-//				} 
-//			});
-//
-//			if (parameterArrayNode != null && parameterArrayNode.size() >= 1) {
-//				gitObjectNode.set(PARAMETERS, parameterArrayNode);
-//				toolConnectorPayloads.add(gitObjectNode);
-//			}
-//		} else {
-//			logger.info("Repo is not provided");
-//		}
-//
-//		if (context.getJenkins() != null && ! context.getJenkins().isEmpty()) {
-//
-//			ObjectNode jenkinsObjectNode = objectMapper.createObjectNode();
-//			jenkinsObjectNode.put(CONNECTOR_TYPE, JENKINS);
-//			ArrayNode parameterArrayNode = objectMapper.createArrayNode();
-//			context.getJenkins().forEach(jenkins -> {
-//				if (jenkins != null && jenkins.getBuildNo() != null && ! jenkins.getBuildNo().isEmpty()) {
-//
-//					parameterArrayNode.add(objectMapper.createObjectNode()
-//							.put(JOB, jenkins.getJobName())
-//							.put(BUILD_ID, jenkins.getBuildNo().trim())
-//							.put(ARTIFACT, jenkins.getArtifact().trim()));
-//				} 
-//			});
-//
-//			if (parameterArrayNode != null && parameterArrayNode.size() >= 1) {
-//				jenkinsObjectNode.set(PARAMETERS, parameterArrayNode);
-//				toolConnectorPayloads.add(jenkinsObjectNode);
-//			}
-//		} else {
-//			logger.info("Jenkins job is not provided");
-//		}
-//
-//		if (context.getBamboo() != null && ! context.getBamboo().isEmpty()) {
-//
-//			ObjectNode bambooNode = objectMapper.createObjectNode();
-//			bambooNode.put(CONNECTOR_TYPE, BAMBOO);
-//			ArrayNode parameterArrayNode = objectMapper.createArrayNode();
-//			context.getBamboo().forEach(bamboo -> {
-//				if (bamboo != null && bamboo.getProjectName() != null && ! bamboo.getProjectName().isEmpty()
-//						&& bamboo.getBuildNumber() != null && bamboo.getPlanName() != null) {
-//
-//					parameterArrayNode.add(objectMapper.createObjectNode()
-//							.put(PROJECT_NAME, bamboo.getProjectName().trim())
-//							.put(PLAN_NAME, bamboo.getPlanName().trim())
-//							.put(BUILD_NUMBER, bamboo.getBuildNumber().trim()));
-//				} 
-//			});
-//
-//			if (parameterArrayNode != null && parameterArrayNode.size() >= 1) {
-//				bambooNode.set(PARAMETERS, parameterArrayNode);
-//				toolConnectorPayloads.add(bambooNode);
-//			}
-//		} else {
-//			logger.info("Bamboo details is not provided");
-//		}
-//
-//		if (context.getAutopilotCanaryId() != null && ! context.getAutopilotCanaryId().isEmpty()) {
-//			ArrayNode commitIds = objectMapper.createArrayNode();
-//			Arrays.asList(context.getAutopilotCanaryId().split(",")).forEach(a -> {
-//				commitIds.add(a.trim());
-//			});
-//
-//			toolConnectorPayloads.add(
-//					objectMapper.createObjectNode().put(CONNECTOR_TYPE, AUTOPILOT)
-//					.set(PARAMETERS, objectMapper.createArrayNode().add(objectMapper.createObjectNode().set(CANARY_ID, commitIds))));
-//		} else {
-//			logger.info("Autopilot id is not provided");
-//		}
-//
-//		if (context.getJfrogWatchName() != null && ! context.getJfrogWatchName().isEmpty()) {
-//			ArrayNode commitIds = objectMapper.createArrayNode();
-//			Arrays.asList(context.getJfrogWatchName().split(",")).forEach(a -> {
-//				commitIds.add(a.trim());
-//			});
-//
-//			toolConnectorPayloads.add(
-//					objectMapper.createObjectNode().put(CONNECTOR_TYPE, JFROG)
-//					.set(PARAMETERS, objectMapper.createArrayNode().add(objectMapper.createObjectNode().set(WATCH_NAME, commitIds))));
-//		} else {
-//			logger.info("Jrog is not provided");
-//		}
-//
-//		if (context.getSonarqubeProjectKey() != null && ! context.getSonarqubeProjectKey().isEmpty()) {
-//			ArrayNode projIds = objectMapper.createArrayNode();
-//			Arrays.asList(context.getSonarqubeProjectKey().split(",")).forEach(a -> {
-//				projIds.add(a.trim());
-//			});
-//
-//			toolConnectorPayloads.add(
-//					objectMapper.createObjectNode().put(CONNECTOR_TYPE, SONARQUBE)
-//					.set(PARAMETERS, objectMapper.createArrayNode().add(objectMapper.createObjectNode().set(PROJECT_KEY, projIds))));
-//		} else {
-//			logger.info("Sonarqube projectid is not provided");
-//		} 
-//
-//		if (context.getAppScanProjectId() != null && ! context.getAppScanProjectId().isEmpty()) {
-//			ArrayNode projIds = objectMapper.createArrayNode();
-//			Arrays.asList(context.getAppScanProjectId().split(",")).forEach(a -> {
-//				projIds.add(a.trim());
-//			});
-//
-//			toolConnectorPayloads.add(
-//					objectMapper.createObjectNode().put(CONNECTOR_TYPE, APPSCAN)
-//					.set(PARAMETERS, objectMapper.createArrayNode().add(objectMapper.createObjectNode().set(REPORT_ID, projIds))));
-//		} else {
-//			logger.info("APPSCAN projectid is not provided");
-//		} 
-//
-//		if (context.getAquaWaveImageId() != null && ! context.getAquaWaveImageId().isEmpty()) {
-//			ArrayNode projIds = objectMapper.createArrayNode();
-//			Arrays.asList(context.getAquaWaveImageId().split(",")).forEach(a -> {
-//				projIds.add(a.trim());
-//			});
-//
-//			toolConnectorPayloads.add(
-//					objectMapper.createObjectNode().put(CONNECTOR_TYPE, AQUAWAVE)
-//					.set(PARAMETERS, objectMapper.createArrayNode().add(objectMapper.createObjectNode().set(IMAGE_ID, projIds))));
-//		} else {
-//			logger.info("AQUAWAVE image is not provided");
-//		} 	
-//
-//		finalJson.set(TOOL_CONNECTOR_PARAMETERS, toolConnectorPayloads);
-//
-//
-//		ArrayNode customArrayNode = objectMapper.createArrayNode();
-//		if (context.getCustomConnector() != null && ! context.getCustomConnector().isEmpty()) {
-//
-//			for (CustomConnector custom : context.getCustomConnector()) {
-//				if (custom != null && custom.getHeader() != null && !custom.getHeader().isEmpty()) {
-//					ObjectNode customNode = objectMapper.createObjectNode();
-//					customNode.put(CONNECTOR_TYPE, CUSTOM);
-//					customNode.put(NAME, custom.getName());
-//
-//					ArrayNode headerNode = objectMapper.createArrayNode();
-//					Arrays.asList(custom.getHeader().split(",")).forEach(a -> {
-//						headerNode.add(a.trim());
-//					});
-//
-//					customNode.set(HEADER_DATA, headerNode);
-//					try {
-//						customNode.set(DATA, objectMapper.readTree(custom.getData()));
-//					} catch (Exception e) {
-//						logger.error("please provide valid json data for custom connector", e);
-//						throw e;
-//					}
-//					customArrayNode.add(customNode);
-//				}
-//			}
-//
-//		} else {
-//			logger.info("CustomConnector is not provided");
-//		}
-//
-//		finalJson.set("customConnectorData", customArrayNode);
-//
-//		return "";
-//	}
 }
