51a2861812d89479d6edca01d1284544066f3dff
Luthan95 <luthan@opsmx.io>
2021-07-27 11:49:45 +0530

    new json changes

--- custom-stage-orca/src/main/java/com/opsmx/plugin/stage/custom/ApprovalTask.java
+++ custom-stage-orca/src/main/java/com/opsmx/plugin/stage/custom/ApprovalTask.java
@@ -233,7 +233,15 @@ public class ApprovalTask implements Task {
 		finalJson.put("approvalCallbackURL", "http://oes-platform:8095/callbackurl");
 		finalJson.put("rejectionCallbackURL", "http://oes-platform:8095/rejectionbackurl");
 		finalJson.put("executionId", executionId);
+		ArrayNode imageIdsNode = objectMapper.createArrayNode();
+		String imageIds = (String) parameterContext.get("imageIds");
+		if (imageIds != null && ! imageIds.isEmpty()) {
+			Arrays.asList(imageIds.split(",")).forEach(tic -> {
+				imageIdsNode.add(tic.trim());
+			});
+		}
 		
+		finalJson.set("imageIds", imageIdsNode);
 		String connectorJson = objectMapper.writeValueAsString(parameterContext.get("connectors"));
 		ArrayNode connectorNode = (ArrayNode) objectMapper.readTree(connectorJson);
 		ArrayNode toolConnectorPayloads = objectMapper.createArrayNode();
